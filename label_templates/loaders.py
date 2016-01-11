99e111d8b805622453b869a8c20293d23a1447f1
Sergei Maertens <sergei@maykinmedia.nl>
2016-01-11 17:31:47 +0100

    Make label_loaders 1.8+ compatible

--- label_templates/loaders.py
+++ label_templates/loaders.py
@@ -7,37 +7,34 @@ import os
 
 from django.conf import settings
 from django.contrib.sites.models import Site
-from django.template.base import TemplateDoesNotExist
-from django.template.loader import BaseLoader, get_template_from_string, find_template_loader, make_origin
+from django.template.base import Template, TemplateDoesNotExist
+from django.template.loaders.base import Loader as BaseLoader
+
+# TODO: subclass from cached loader?
 
 
 class Loader(BaseLoader):
     is_usable = True
 
-    def __init__(self, loaders):
-        self._loaders = loaders
-        self._cached_loaders = []
-
-    @property
-    def loaders(self):
-        # Resolve loaders on demand to avoid circular imports
-        if not self._cached_loaders:
-            # Set self._cached_loaders atomically. Otherwise, another thread
-            # could see an incomplete list. See #17303.
-            cached_loaders = []
-            for loader in self._loaders:
-                cached_loaders.append(find_template_loader(loader))
-            self._cached_loaders = cached_loaders
-        return self._cached_loaders
+    def __init__(self, engine, loaders):
+        self.template_cache = {}
+        self.find_template_cache = {}
+        self.loaders = engine.get_template_loaders(loaders)
+        super(Loader, self).__init__(engine)
 
     def find_template(self, names, dirs=None):
+        """
+        Helper method. Lookup the template :param name: in all the configured loaders.
+        """
         for loader in self.loaders:
             for name in names:
                 try:
                     template, display_name = loader(name, dirs)
-                    return (template, make_origin(display_name, loader, name, dirs))
                 except TemplateDoesNotExist:
                     pass
+                else:
+                    origin = self.engine.make_origin(display_name, loader, name, dirs)
+                    return template, origin
         raise TemplateDoesNotExist(', '.join(names))
 
     def load_template(self, template_name, template_dirs=None):
@@ -52,7 +49,7 @@ class Loader(BaseLoader):
         template, origin = self.find_template(templates, template_dirs)
         if not hasattr(template, 'render'):
             try:
-                template = get_template_from_string(template, origin, template_name)
+                template = Template(template, origin, template_name, self.engine)
             except TemplateDoesNotExist:
                 # If compiling the template we found raises TemplateDoesNotExist,
                 # back off to returning the source and display name for the template
